79da0f0f7522a6dc236fdac55f29e88178cc1962
magic-sword <magic.sword.0001@gmail.com>
2023-10-16 02:19:41 +0900

    全結合層を追加

--- pytorch_dmbn/nn/blending_encoder.py
+++ pytorch_dmbn/nn/blending_encoder.py
@@ -25,7 +25,9 @@ from .time_distributed import TimeDistributed
 class BlendingEncoder(nn.Module):
     def __init__(
             self,
-            encoders = []
+            encoders = [],
+            in_features = 129,
+            linear_features = 1024,
             ):
         """BlendingEncoder
 
@@ -36,16 +38,18 @@ class BlendingEncoder(nn.Module):
 
         Args:
             encoders (list): nn.Moduleエンコーダーのリスト
+            in_features (int): エンコーダーの出力特徴数
+            linear_features (int): 最終出力で実行する全結合層の特徴数
 
         Shape:
             Inputs: :math:`[(N, L1_{in}), (N, L2_{in}), ...]`
-            Output: :math:`(N, L_{out}+1)`
+            Output: :math:`(N, C_{out}+1)`
             , where
 
         .. math::
             N = バッチデータ数
             L1_{in} = 1つめのエンコーダーの入力
-            L_{out} = 全てのエンコーダーの出力
+            L_{out} = 最終出力の全結合層の特徴数(inear_features)
 
         Examples:
             >>> encoders = [
@@ -53,7 +57,7 @@ class BlendingEncoder(nn.Module):
                 LinearEncoder(channels = [8,32,64,64,128,128,256,128])
             ]
             >>> encoders = [TimeAverageEncoder(encoder) for encoder in encoders]
-            >>> m = BlendingEncoder(encoders)
+            >>> m = BlendingEncoder(encoders, in_features=129, linear_features=1024,)
 
             >>> inputs = [
                 torch.rand(32, 10, 3, 128, 128),
@@ -61,14 +65,22 @@ class BlendingEncoder(nn.Module):
             ]
             >>> output = m(inputs=inputs, coefficients=[1,0], target=0.1)
             >>> print(output.shape)
-                torch.Size([32, 129])
+                torch.Size([32, 1024])
 
         Note:
-            注意事項などを記載
+            * エンコーダーから出力形は、バッチ数、時間ステップ、チャンネルの3次元である必要がある
+                * (N, T, C)
+                * ここで、チャンネルの数は全てのエンコーダで統一すること
+            * in_featuresは、エンコーダの出力チャンネルに、ターゲット時間を追加した値となる
+                * in_features = C + 1
 
         """
         super().__init__()
         self.encoders = nn.ModuleList(encoders)
+        
+        # 全結合層
+        self.linear = nn.Linear(in_features=in_features, out_features=linear_features)
+        self.relu = nn.ReLU()
 
     def forward(self, inputs=[], coefficients=[], target=0):
         # エンコーダーを実行
@@ -95,5 +107,9 @@ class BlendingEncoder(nn.Module):
         target_shape = general_representation.shape[:-1] + (1,)
         target_tensor = torch.full(target_shape, target)
         x = torch.concatenate((general_representation, target_tensor), dim=-1)
+
+        # 全結合層
+        x = self.linear(x)
+        x = self.relu(x)
 
         return x
