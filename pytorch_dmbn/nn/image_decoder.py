5d18c6ba1e3807fc119e83420a3e4a568947ed1d
magic-sword <magic.sword.0001@gmail.com>
2023-12-10 01:31:12 +0900

    ImageDecoderから全結合層を削除

--- pytorch_dmbn/nn/image_decoder.py
+++ pytorch_dmbn/nn/image_decoder.py
@@ -19,8 +19,7 @@ import torch.nn as nn
 class ImageDecoder(nn.Module):
     def __init__(
         self,
-        in_features = 128,
-        linear_features = 1024,
+        in_features = 1024,
         channels = [256,128,128,64,64,32,16,8,3], 
         conv_kernel_size = 3,
         upsample_scale = 2,
@@ -53,8 +52,7 @@ class ImageDecoder(nn.Module):
         Examples:
             >>> inputs = torch.rand(32, 128)
             >>> image_decoder = ImageEncoder(
-                    in_features = 128,
-                    linear_features = 1024,
+                    in_features = 1024,
                     channels = [256,128,128,64,64,32,16,8,3], 
                     conv_kernel_size = 3,
                     upsample_scale = 2,
@@ -75,14 +73,10 @@ class ImageDecoder(nn.Module):
         # 出力画像の縦横比と、入力特徴数から、初期の画像サイズを逆算する
         image_size_h, image_size_w = image_size
         height_rate = image_size_h / image_size_w # 出力画像の縦横比
-        initial_image_area = linear_features / channels[0] # 初期の画像面積
+        initial_image_area = in_features / channels[0] # 初期の画像面積
         initial_width = numpy.sqrt(initial_image_area / height_rate) # 初期の画像の横幅
         initial_height =  initial_image_area / initial_width # 初期の画像の高さ
         self.initial_image_size = (channels[0], int(initial_height), int(initial_width)) # 入力特徴数を初期の形に変形
-        
-        # 全結合層
-        self.linear = nn.Linear(in_features=in_features, out_features=linear_features)
-        self.relu = nn.ReLU()
         
         # デコードレイヤー層を構築する
         size_h = initial_height
@@ -109,9 +103,6 @@ class ImageDecoder(nn.Module):
         self.decoder_layers = nn.ModuleList(layers)
 
     def forward(self, x):
-        x = self.linear(x)
-        x = self.relu(x)
-        
         # 入力特徴数を初期の形に変形
         x = x.reshape(-1, *self.initial_image_size)
         
